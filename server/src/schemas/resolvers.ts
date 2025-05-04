29c02ba9e58f33505ef4ab7f236a47b0ab805e81
unknown <ernestoaleman00@gmail.com>
2025-05-04 02:54:41 -0500

    Added Query for resolvers. Mutations to follow

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -6,11 +6,19 @@ import { signToken, AuthenticationError } from '../services/auth.js';
 
 
 const resolvers = {
+  Query: {
+    me: async (_parent: any, _args: any, context: IUserContext): Promise<IUserDocument | null> => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+        return userData;
+      }
+      throw new AuthenticationError('User not authenticated');
+    },
+  },
 
 
 
 
-    
 
 
 }
