e443c5e4a3171d65bf822ea9b7b6dc21e559ec17
unknown <ernestoaleman00@gmail.com>
2025-05-04 03:45:33 -0500

    Init return statement for SearchBook Page function

--- client/src/pages/SearchBooks.tsx
+++ client/src/pages/SearchBooks.tsx
@@ -84,24 +84,77 @@ const SearchBooks = () => {
     }
   };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   return (
+    <>
+      <div className="text-light bg-dark p-5">
+        <Container>
+          <h1>Search for Books!</h1>
+          <Form onSubmit={handleFormSubmit}>
+            <Row>
+              <Col xs={12} md={8}>
+                <Form.Control
+                  name="searchInput"
+                  value={searchInput}
+                  onChange={(e) => setSearchInput(e.target.value)}
+                  type="text"
+                  size="lg"
+                  placeholder="Search for a book"
+                />
+              </Col>
+              <Col xs={12} md={4}>
+                <Button type="submit" variant="success" size="lg">
+                  Submit Search
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Container>
+      </div>
 
-  )
-
-}
+      <Container>
+        <h2 className='pt-5'>
+          {searchedBooks.length
+            ? `Viewing ${searchedBooks.length} results:`
+            : 'Search for a book to begin'}
+        </h2>
+        <Row>
+          {searchedBooks.map((book: Book) => {
+            return (
+              <Col md="4" key={book.bookId}>
+                <Card border="dark" className='mb-3'>
+                  {book.image ? (
+                    <Card.Img
+                      src={book.image}
+                      alt={`The cover for ${book.title}`}
+                      variant="top"
+                    />
+                  ) : null}
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <p className="small">Authors: {book.authors}</p>
+                    <Card.Text>{book.description}</Card.Text>
+                    {Auth.loggedIn() && (
+                      <Button
+                        disabled={savedBookIds?.some(
+                          (savedId: string) => savedId === book.bookId
+                        )}
+                        className="btn-block btn-info"
+                        onClick={() => handleSaveBook(book.bookId)}
+                      >
+                        {savedBookIds?.some((savedId: string) => savedId === book.bookId)
+                          ? 'Book Already Saved!'
+                          : 'Save This Book!'}
+                      </Button>
+                    )}
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </>
+  );
+};
 
 export default SearchBooks;
