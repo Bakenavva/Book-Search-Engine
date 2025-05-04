e16a973a3e5c13dbbbcc12c499212f84d0b06acc
unknown <ernestoaleman00@gmail.com>
2025-05-04 02:59:49 -0500

    In typeDefs, defined type User

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,7 +1,13 @@
 import { gql } from 'graphql-tag';
 
 const typeDefs = gql`
-
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
 
 
